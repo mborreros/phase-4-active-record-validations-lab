534f0d606cd13af3398ba14273c1dc41c27e91e8
mborreros <mlbsindia@gmail.com>
2022-05-05 16:38:33 -0400

    commit tree 2e1061e82add7fdf81b6b77111e3ddd3ec73fc34

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+validates :title, presence: true, exclusion: ["True Facts"]
+validates :content, length: { minimum: 250 }
+validates :summary, length: { maximum: 250 }
+validates :category, inclusion: ["Fiction", "Non-Fiction"]
+
 end
